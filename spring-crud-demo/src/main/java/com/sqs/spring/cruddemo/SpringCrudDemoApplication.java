b4a6d56afedc7fe3ea671b8265cc2c099a56ec32
rsnf <roberto.sarquis@gmail.com>
2023-07-18 20:41:16 -0300

    CRUD

--- spring-crud-demo/src/main/java/com/sqs/spring/cruddemo/SpringCrudDemoApplication.java
+++ spring-crud-demo/src/main/java/com/sqs/spring/cruddemo/SpringCrudDemoApplication.java
@@ -1,5 +1,7 @@
 package com.sqs.spring.cruddemo;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,12 +20,95 @@ public class SpringCrudDemoApplication {
     @Bean
     CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 	return runner -> {
+
 	    // createStudent(studentDAO);
 
-	    createMultipleStudents(studentDAO);
+	    // createMultipleStudents(studentDAO);
+
+	    // readStudent(studentDAO);
+
+	    // queryForStudents(studentDAO);
+
+	    // queryForStudentsByLastName(studentDAO);
+
+	    // updateStudent(studentDAO);
+
+	    // deleteStudent(studentDAO);
+
+	    deleteAllStudents(studentDAO);
 	};
     }
 
+    private void deleteAllStudents(StudentDAO studentDAO) {
+
+	System.out.println("Deleting all students");
+	int numRowsDeleted = studentDAO.deleteAll();
+	System.out.println("Deleted row count: " + numRowsDeleted);
+    }
+
+    @SuppressWarnings("unused")
+    private void deleteStudent(StudentDAO studentDAO) {
+
+	int studentId = 3;
+	System.out.println("Deleting student id: " + studentId);
+	studentDAO.delete(studentId);
+    }
+
+    @SuppressWarnings("unused")
+    private void updateStudent(StudentDAO studentDAO) {
+
+	int studentId = 1;
+	System.out.println("Getting student with id: " + studentId);
+	Student myStudent = studentDAO.findById(studentId);
+	System.out.println(myStudent);
+
+	System.out.println("Updating student ...");
+	// myStudent.setFirstName("Scooby");
+	myStudent.setFirstName("John");
+	studentDAO.update(myStudent);
+
+	System.out.println("Updated student: " + myStudent);
+    }
+
+    @SuppressWarnings("unused")
+    private void queryForStudentsByLastName(StudentDAO studentDAO) {
+
+	List<Student> theStudents = studentDAO.findByLastName("Duck");
+
+	for (Student tempStudent : theStudents) {
+	    System.out.println(tempStudent);
+	}
+    }
+
+    @SuppressWarnings("unused")
+    private void queryForStudents(StudentDAO studentDAO) {
+
+	List<Student> theStudents = studentDAO.findAll();
+
+	for (Student tempStudent : theStudents) {
+	    System.out.println(tempStudent);
+	}
+    }
+
+    @SuppressWarnings("unused")
+    private void readStudent(StudentDAO studentDAO) {
+
+	System.out.println("Creating new student object ...");
+	Student tempStudent = new Student("Daffy", "Duck", "daffy@sqs.com");
+
+	System.out.println("Saving the student ...");
+	studentDAO.save(tempStudent);
+
+	int theId = tempStudent.getId();
+	System.out.println("Saved student. Generated id: " + theId);
+
+	System.out.println("Retrievind student with id: " + theId);
+	Student myStudent = studentDAO.findById(theId);
+
+	System.out.println("Found the student: " + myStudent);
+    }
+
+    @SuppressWarnings("unused")
     private void createMultipleStudents(StudentDAO studentDAO) {
 	System.out.println("Creating 3 new students objects ...");
 	Student tempStudent1 = new Student("John", "Doe", "john@sqs.com");
--- spring-crud-demo/src/main/java/com/sqs/spring/cruddemo/dao/StudentDAOImpl.java
+++ spring-crud-demo/src/main/java/com/sqs/spring/cruddemo/dao/StudentDAOImpl.java
@@ -1,11 +1,14 @@
 package com.sqs.spring.cruddemo.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.sqs.spring.cruddemo.entity.Student;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -20,6 +23,55 @@ public class StudentDAOImpl implements StudentDAO {
     @Transactional
     public void save(Student theStudent) {
 	entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+	return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+
+	// TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student ORDER
+	// BY lastName asc", Student.class);
+
+	TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+	return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+
+	TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+
+	theQuery.setParameter("theData", theLastName);
+
+	return theQuery.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student theStudent) {
+	entityManager.merge(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+
+	Student theStudent = entityManager.find(Student.class, id);
+
+	entityManager.remove(theStudent);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+
+	int numRowsDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+	return numRowsDeleted;
     }
 
 }
